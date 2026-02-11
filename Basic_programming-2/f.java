2b7d8d865519dceb7282ab30197fcf29b64b7f83
Muskanpatel933 <muskan933patel@gmail.com>
2026-02-11 12:24:09 +0530

    [Muskan] commit

--- Basic_programming-2/f.java
+++
@@ -1,9 +0,0 @@
-class Bridgelabz
-{
-public static void main(String args [])
-{
-   int celsius=2;
-    double f = (celsius*9/5)+32;
-     System.out.println("temperature of fahrenheit :"+ f);
-  }
-}
